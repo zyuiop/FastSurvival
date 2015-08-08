87fd7272ac10c67ed04e50a59f413d9209a01bc1
zyuiop <zyuiop@zyuiop.net>
2015-08-08 13:49:00 +0200

    Updated License & fixed generation issues

--- src/main/java/net/zyuiop/fastsurvival/generation/WorldGenerator.java
+++ src/main/java/net/zyuiop/fastsurvival/generation/WorldGenerator.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 zyuiop
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package net.zyuiop.fastsurvival.generation;
 
 import org.bukkit.Bukkit;
@@ -16,8 +40,13 @@ public class WorldGenerator implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onWorldInit(final WorldInitEvent event) {
-		if (event.getWorld().getEnvironment() == World.Environment.NORMAL) {
-			event.getWorld().getPopulators().add(populator);
+		loadWorld(event.getWorld());
+	}
+
+	private void loadWorld(World world) {
+		if (world.getEnvironment() == World.Environment.NORMAL) {
+			world.getPopulators().add(populator);
+			Bukkit.getLogger().info("Added custom generation to world " + world.getName());
 		}
 	}
 
@@ -71,6 +100,6 @@ public class WorldGenerator implements Listener {
 			}
 		}
 
-
+		Bukkit.getWorlds().forEach(this::loadWorld);
 	}
 }
